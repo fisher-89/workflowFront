dd08340cf99cd55c3a585f9ef053aa56be92d366
weiyingHappy <949736522@qq.com>
2018-10-25 18:09:43 +0800

    流程图

--- src/routes/TableEdit/test2.js
+++ src/routes/TableEdit/test2.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { remove, findIndex, last } from 'lodash';
+import style from './index.less'
 
 const horizontal = 30;
 const startPoint = [30, 30];
@@ -57,38 +58,46 @@ const testData = [
     ],
   },
 ];
-const curveRadius = 0.4;
+const curveRadius = 4;
 const nodeRadius = 5;
-const colGap = 40;
-const line = {
-  color: 'black',
+const colGap = 20;
+const verticalRate = 30;
+const lineStyle = {
+  color: 'rgb(202,233,233)',
   width: 1,
 };
+const firstColLineStyle = {
+  color: 'rgb(202,233,233)',
+  width: 2,
+}
 const node = {
   radius: 5,
-  color: 'red',
+  color: 'rgb(89,195,195)',
   width: 1
 }
+const firstNode = {
+  radius: 5,
+  color: 'rgb(89,195,195)',
+};
 const text = {
   size: 12, color: 'black'
 }
-const test = [
-  { id: 1, time: '1', next: [2, 3, 4], prev: [] },
-  { id: 2, time: '2', next: [8], prev: [1] },
-  { id: 3, time: '3', next: [5, 6], prev: [1] },
-  { id: 4, time: '4', next: [8], prev: [1] },
-  { id: 5, time: '5', next: [7], prev: [3] },
-  { id: 6, time: '6', next: [7], prev: [3] },
-  { id: 7, time: '7', next: [8], prev: [5, 6] },
-  { id: 8, time: '8', next: [9, 10], prev: [2, 4, 7] },
-  { id: 9, time: '9', next: [11], prev: [8] },
-  { id: 10, time: '10', next: [11], prev: [8] },
-  { id: 11, time: '11', next: [12, 13], prev: [9, 10] },
-  { id: 12, time: '9', next: [14], prev: [11] },
-  { id: 13, time: '10', next: [14], prev: [11] },
-  { id: 14, time: '11', next: [], prev: [12, 13] },
-
-];
+// const test = [
+//   { id: 1, time: '1', next: [2, 3, 4], prev: [] },
+//   { id: 2, time: '2', next: [8], prev: [1] },
+//   { id: 3, time: '3', next: [5, 6], prev: [1] },
+//   { id: 4, time: '4', next: [8], prev: [1] },
+//   { id: 5, time: '5', next: [7], prev: [3] },
+//   { id: 6, time: '6', next: [7], prev: [3] },
+//   { id: 7, time: '7', next: [8], prev: [5, 6] },
+//   { id: 8, time: '8', next: [9, 10], prev: [2, 4, 7] },
+//   { id: 9, time: '9', next: [11], prev: [8] },
+//   { id: 10, time: '10', next: [11], prev: [8] },
+//   { id: 11, time: '11', next: [12, 13], prev: [9, 10] },
+//   { id: 12, time: '9', next: [14], prev: [11] },
+//   { id: 13, time: '10', next: [14], prev: [11] },
+//   { id: 14, time: '11', next: [], prev: [12, 13] },
+// ];
 
 // const test = [
 //   { id: 1, time: '1', next: [2, 3, 4], prev: [] },
@@ -98,29 +107,39 @@ const test = [
 //   { id: 5, time: '5', next: [6], prev: [2, 3] },
 //   { id: 6, time: '6', next: [], prev: [4, 5] },
 // ];
-// const test = [
-//   { id: 1, time: '1', next: [2, 3, 4], prev: [] },
-//   { id: 2, time: '2', next: [5, 6], prev: [1] },
-//   { id: 3, time: '3', next: [7, 8], prev: [1] },
-//   { id: 4, time: '4', next: [9, 10], prev: [1] },
-//   { id: 5, time: '5', next: [11], prev: [2] },
-//   { id: 6, time: '6', next: [11], prev: [2] },
-//   { id: 7, time: '6', next: [14], prev: [3] },
-//   { id: 8, time: '6', next: [11], prev: [3] },
-//   { id: 9, time: '6', next: [14], prev: [4] },
-//   { id: 10, time: '6', next: [11], prev: [4] },
-//   { id: 11, time: '6', next: [14], prev: [6, 8, 10] },
-//   { id: 12, time: '6', next: [], prev: [5, 11, 7, 9] },
-// ];
+const test = [
+  { id: 1, time: '1', next: [2, 3, 4], prev: [], },
+  { id: 2, time: '2', next: [5, 6], prev: [1] },
+  { id: 3, time: '3', next: [7, 8], prev: [1] },
+  { id: 4, time: '4', next: [9, 10], prev: [1] },
+  { id: 5, time: '5', next: [12], prev: [2] },
+  { id: 6, time: '6', next: [11], prev: [2] },
+  { id: 7, time: '6', next: [13], prev: [3] },
+  { id: 8, time: '6', next: [11], prev: [3] },
+  { id: 9, time: '6', next: [], prev: [4] },
+  { id: 10, time: '6', next: [11], prev: [4] },
+  { id: 11, time: '6', next: [13], prev: [6, 8, 10] },
+  { id: 12, time: '6', next: [13], prev: [5] },
+  { id: 13, time: '6', next: [], prev: [12, 11, 7] },
+];
 const cols = {};
 const testKeyById = {};
 const lines = [];
 const rows = [];
 let uniqueRows = [];
+let curves = [];
 const unfinishedLines = [];
-let colIndex = 1;
 @connect()
 export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: [],
+      lines: [],
+      rows: [],
+      uniqueRows: [],
+    }
+  }
   componentDidMount() {
     this.canvas = document.getElementById('myCanvas');
     this.ctx = this.canvas.getContext('2d');
@@ -143,8 +162,6 @@ export default class Test extends Component {
           this.finishColLine(prevStep.line, step.y - 0.5); //上一条线的结束点
           max = colIndex - max >= 0 ? colIndex : max;
           min = colIndex - min <= 0 ? colIndex : min;
-          // maxIndex = index - maxIndex >= 0 ? index : maxIndex;
-          // minIndex = index - minIndex <= 0 ? index : minIndex;
         });
         const row = this.createNewRowLine(min, max, step.y - 0.5);
         step.row = row;
@@ -155,7 +172,7 @@ export default class Test extends Component {
         });
         const prevLines = prevSteps.map(item => item.line);
         const basicLine = this.findBasicLine(prevLines);
-        const newLine = this.createNewColLine(basicLine.col, basicLine.colIndex, step.y - 0.5, prevLines, step.y);
+        const newLine = this.createNewColLine(basicLine.col, basicLine.colIndex, step.y - 0.5, prevLines);
         step.line = newLine;
         step.unfinishedLines = [...unfinishedLines];
       } else {//不分叉
@@ -164,11 +181,9 @@ export default class Test extends Component {
         let min = '1';
         if (prevStep.next.length > 1) {
           const subIndex = prevStep.next.indexOf(step.id);
-
-          const newLine = this.separateColLine(prevStep.line, prevStep.y + 0.5, subIndex, step.y);
+          const newLine = prevStep.line.next[subIndex];
           step.line = newLine;
           prevStep.next.forEach((next, i) => {
-            // const curLine = this.separateColLine(prevStep.line, prevStep.y + 0.5, i);
             const colIndex = `${prevStep.line.colIndex}${i >= 10 ? i : `0${i}`}`;
             max = colIndex - max >= 0 ? colIndex : max;
             min = colIndex - min <= 0 ? colIndex : min;
@@ -178,32 +193,40 @@ export default class Test extends Component {
           step.unfinishedLines = [...unfinishedLines];
         } else {
           step.line = prevStep.line;
-          step.unfinishedLines = prevStep.unfinishedLines;
+          step.unfinishedLines = [...unfinishedLines];
         }
       }
+
       // 生成cols分支
       if (step.next.length > 1) {
-        this.finishColLine(step.line, step.y + 0.5);
-        step.next.forEach((next_id, next_index) => {
-          step.line.col[next_index] = step.line.col[next_index] || {};
-        });
+        this.separateColLine(step, step.y + 0.5)
+      }
+      if (step.next.length === 0) {
+        this.finishColLine(step.line, step.y);
       }
     });
-    this.fillColsIndex(cols);
+    const maxIndex = this.fillColsIndex(cols);
     console.log('test:', test);
     console.log('cols:', cols);
     console.log('lines:', lines);
     console.log('rows:', rows);
+
     const endPoint = last(test);
     const { y } = endPoint;
-    console.log(y, endPoint)
-    this.canvas.height = (y + 0.5 + 8) * 30;
-
+    this.canvas.height = (y + 0.5) * verticalRate + 40;
+    this.canvas.width = maxIndex * colGap + 20;
+    this.drawRect(colGap, verticalRate, maxIndex * colGap + 20, (y + 0.5) * verticalRate + 40)
     this.recombineRows();
+    this.setState({
+      test,
+      lines,
+      rows,
+      uniqueRows,
+    })
     this.draw();
   }
 
-  recombineRows = () => {
+  recombineRows = () => {//uniqueRows
     let obj = {};
     let min = 1;
     let max = 1;
@@ -230,8 +253,24 @@ export default class Test extends Component {
         min = start - min <= 0 ? start : min;
         max = end - max > 0 ? end : max;
       })
-      uniqueRows.push({ start: min, end: max, y: key })
+      uniqueRows.push({ start: min, end: max, y: key });
+      this.makeCurves(min, max, key)
     })
+  }
+
+  makeCurves = (start, end, y) => {//curves 
+    const startPoint = { x: start, y };
+    const line = lines.filter(line => (`${line.col.index}` === `${end}` && (`${y}` === `${line.start}` || `${y}` === `${line.end}`)));
+    const endPoint = { x: end, y: line[0].end };
+    let p1 = { x: end - (curveRadius / colGap), y };
+    let p2 = { x: end, y: y - 0 + (curveRadius / verticalRate) };
+    let direction = 1;
+    if ((startPoint.x - endPoint.x) * (startPoint.y - endPoint.y) <= 0) {
+      p1 = { x: end - (curveRadius / colGap), y };
+      p2 = { x: end, y: y - (curveRadius / verticalRate) };
+      direction = -1;
+    }
+    curves.push({ start: p1, end: p2, direction });
   }
 
   createNewRowLine = (min, max, y) => {
@@ -240,10 +279,12 @@ export default class Test extends Component {
     return row;
   }
 
-  createNewColLine = (col, colIndex, start = 1, prev = [], y = 1) => {
-    const line = { col, colIndex, start, end: '', prev, next: [], crossingPoint: [], pointY: y };
+  createNewColLine = (col, colIndex, start = 1, prev = []) => {
+    const line = { col, colIndex, start, end: '', prev, next: [], crossingPoint: [] };
     lines.push(line);
-    // unfinishedLines.push(line);
+    prev.forEach((prevLine) => {
+      prevLine.next.push(line);
+    });
     this.sortUnfinishedLines(line);
     return line;
   }
@@ -258,23 +299,14 @@ export default class Test extends Component {
   /**
    * 生成线分支
    */
-  separateColLine = (prevLine, startY, subIndex, pointY) => {
-    const col = prevLine.col[subIndex];
-    const line = {
-      col,
-      colIndex: `${prevLine.colIndex}${subIndex >= 10 ? subIndex : `0${subIndex}`}`,
-      start: startY,
-      end: '',
-      prev: [prevLine],
-      next: [],
-      crossingPoint: [],
-      pointY
-    };
-    prevLine.next.push(line);
-    lines.push(line);
-    // unfinishedLines.push(line);
-    this.sortUnfinishedLines(line);
-    return line;
+  separateColLine = (prevStep, startY) => {
+    const prevLine = prevStep.line;
+    this.finishColLine(prevLine, startY);
+    prevStep.next.forEach((next, subIndex) => {
+      const col = prevLine.col[subIndex] = prevLine.col[subIndex] || {};
+      const colIndex = `${prevLine.colIndex}${subIndex >= 10 ? subIndex : `0${subIndex}`}`;
+      this.createNewColLine(col, colIndex, startY, [prevLine]);
+    });
   }
 
   findBasicLine = (prevLines) => {
@@ -339,29 +371,26 @@ export default class Test extends Component {
     });
   }
 
-  fillColsIndex = (cols) => {
+  fillColsIndex = (cols, colIndex = 1) => {
     const index = colIndex;
     Object.keys(cols).forEach((key) => {
       if (key > 0) colIndex += 1;
-      this.fillColsIndex(cols[key]);
+      colIndex = this.fillColsIndex(cols[key], colIndex);
     });
     cols.index = index;
+    return colIndex;
   }
 
-  vertivalGap = (start, end, height) => {
-    const interval = (end - start) / height;
-  }
-
-  drawRect = () => {
+  drawRect = (x, y, w, h) => {
     if (this.canvas) {
       const { ctx } = this;
       ctx.beginPath();
-      ctx.rect(5, 5, 290, 140);
-      ctx.stroke();
+      ctx.fillStyle = "#fff";
+      ctx.fillRect(x, y, w, h);
     }
   }
 
-  drawLine = (x1, y1, x2, y2, color = 'black') => {
+  drawLine = (x1, y1, x2, y2, color) => {
     if (this.canvas) {
       const { ctx } = this;
       ctx.beginPath();
@@ -377,7 +406,7 @@ export default class Test extends Component {
     const { ctx } = this;
     ctx.beginPath();
     ctx.arc(x, y, r, sAngle, eAngle, counterclockwise);
-    // ctx.closePath();
+    ctx.closePath();
     if (fill) {
       ctx.fillStyle = color;
       ctx.fill();
@@ -393,16 +422,14 @@ export default class Test extends Component {
       const { ctx } = this;
       ctx.font = '12px Georgia';
       ctx.fillStyle = 'red';
-
       ctx.fillText(str, x, y);
-
     }
   }
 
-  drawCrossPoint = (crossingPoint, index) => {
+  drawCrossPoint = (crossingPoint, index, c1, c2) => {
     crossingPoint.forEach(point => {
-      this.drawArc((index * colGap) - 1, point * colGap, 6, -0.5 * Math.PI, 0.5 * Math.PI, 'black', );
-      this.drawArc((index * colGap) - 1, point * colGap, 4, -0.5 * Math.PI, 0.5 * Math.PI, '#fff', );
+      this.drawArc((index * colGap) - 1, point * verticalRate, 5, -0.5 * Math.PI, 0.5 * Math.PI, c1);
+      this.drawArc((index * colGap) - 1, point * verticalRate, 4, -0.5 * Math.PI, 0.5 * Math.PI, c2);
     })
   }
 
@@ -410,66 +437,96 @@ export default class Test extends Component {
     this.drawArc(x, y, r1, 0, 2 * Math.PI, c1);
     this.drawArc(x, y, r2, 0, 2 * Math.PI, c2);
   }
-  drawCurve = (x1, y1, x2, y2, r, direction) => {
-    // if (this.canvas) {
-    //   const { ctx } = this;
-    //   ctx.beginPath();
-    //   ctx.moveTo(x1 - 0.1, y1);
-    //   ctx.lineTo(x1, y1);
-    //   ctx.arcTo(x1, y1, x2, y2, r); // 创建弧
-    //   // ctx.arcTo(150,20,150,70,50); // 创建弧
-    //   // 150,20,150,70,50
-    //   ctx.lineTo(x2, direction > 0 ? y2 + 0.1 : y2 - 0.1);
-    //   ctx.stroke();
-    // }
-    let c = { x: x1, y: y1 - 0 + curveRadius };
-    if (direction < 0) {
-      c = { x: x1, y: y1 - curveRadius };
-    }
-    this.drawLine(x1 * colGap, y1 * colGap, x2 * colGap, y1 * colGap, '#fff');
-    this.drawLine(x2 * colGap, y1 * colGap, x2 * colGap, y2 * colGap, '#fff');
-    this.drawArc(c.x * colGap, c.y * colGap, curveRadius * colGap, 0, direction > 0 ? -0.5 * Math.PI : 0.5 * Math.PI, 'black', !!(direction > 0), false);
+
+  drawGeneralArc = (x, y, r, color) => {
+    console.log('drawGeneralArc', x, y, color)
+    this.drawArc(x, y, r, 0, 2 * Math.PI, color, false)
   }
 
+  drawCurve = (x1, y1, x2, y2, direction, r = curveRadius) => {
+    let c = { x: x1, y: y1 - 0 + (r / verticalRate) };
+    if (direction < 0) {
+      c = { x: x1, y: y1 - (r / verticalRate) };
+    }
+    this.drawLine(x1 * colGap, y1 * verticalRate, x2 * colGap, y1 * verticalRate, '#fff');
+    this.drawLine(x2 * colGap, y1 * verticalRate, x2 * colGap, y2 * verticalRate, '#fff');
+    this.drawArc(c.x * colGap, c.y * verticalRate, r, 0, direction > 0 ? -0.5 * Math.PI : 0.5 * Math.PI, lineStyle.color, !!(direction > 0));
+  }
 
   draw = () => {
-    const curves = [];
     uniqueRows.forEach((row) => {
       const { start, end, y } = row;
-      this.drawLine(start * colGap, y * colGap, end * colGap, y * colGap);
-      const startPoint = { x: start, y };
-      const line = lines.filter(line => (`${line.col.index}` === `${end}` && (`${y}` === `${line.start}` || `${y}` === `${line.end}`)));
-      const endPoint = { x: end, y: line[0].end };
-      let p1 = { x: end - curveRadius, y };
-      let p2 = { x: end, y: y - 0 + curveRadius };
-      let direction = 1;
-      if ((startPoint.x - endPoint.x) * (startPoint.y - endPoint.y) <= 0) {
-        p1 = { x: end - curveRadius, y };
-        p2 = { x: end, y: y - curveRadius };
-        direction = -1;
-      }
-      curves.push({ start: p1, end: p2, direction });
+      this.drawLine(start * colGap, y * verticalRate, end * colGap, y * verticalRate, lineStyle.color);
     })
-    lines.forEach((line) => {
+    lines.forEach((line, i) => {
+      // || test[test.length - 1].y + 0.5
       const { col: { index }, start, end, pointY, crossingPoint } = line;
-      this.drawLine(index * colGap, start * colGap, index * colGap, (end || test[test.length - 1].y + 0.5) * colGap);
-      this.drawRing(index * colGap, pointY * colGap, nodeRadius, 3, node.color, '#fff');
-      this.drawText(pointY, index * 32, pointY * colGap);
+      const color = index === 1 ? firstColLineStyle.color : lineStyle.color;
+      this.drawLine(index * colGap, start * verticalRate, index * colGap, end * verticalRate, color);
       if (crossingPoint.length) {
-        this.drawCrossPoint(crossingPoint, index);
+        this.drawCrossPoint(crossingPoint, index, lineStyle.color, '#fff');
       }
     })
+    test.forEach((point) => {
+      const { line: { col: { index } }, y } = point;
+      if (y === 1) {
+        this.drawRing(index * colGap, y * verticalRate, node.radius, node.radius - 1, node.color, '#fff');
+      }
+      else {
+        this.drawGeneralArc(index * colGap, y * verticalRate, node.radius, node.color)
+      }
+    });
     curves.forEach((p) => {
       const { start, end, direction } = p;
-      this.drawCurve(start.x, start.y, end.x, end.y, curveRadius, direction)
+      this.drawCurve(start.x, start.y, end.x, end.y, direction)
     })
+  }
+
+  renderTimeLine = () => {
+    const { test } = this.state;
+    const timelines = test.map(line => {
+      const { unfinishedLines = [], y, id } = line;
+      const lastLines = last(unfinishedLines)
+      const maxColIndex = lastLines ? lastLines.col.index : 0;
+      const style = {
+        position: 'absolute',
+        left: maxColIndex * colGap + 14,
+        right: 15,
+        top: y * verticalRate - 11,
+        height: '22px', lineHeight: '22px',
+        display: 'flex',
+        justifyContent: 'space-between',
+
+      }
+      const fisrtDivStyle = {
+        fontSize: '14px',
+        color: 'rgb(102,102,102)'
+      }
+      const timeStyle = {
+        fontSize: '12px',
+        color: 'rgb(136,136,136)',
+
+      }
+      return (
+        <div style={{ ...style, background: '#fff' }} key={id} >
+          <div>
+            <span style={{ ...fisrtDivStyle }}>{id}</span>
+            <span style={{ ...fisrtDivStyle, marginLeft: '10px' }}>通过</span>
+          </div>
+          <div style={{ ...timeStyle }} >{id}</div>
+        </div>
+      )
+    })
+    return timelines
   }
 
   render() {
-
+    const { test } = this.state;
     return (
-      <div>
+      <div className={style.flow_chart}>
         <canvas id="myCanvas" width="300" height="600" />
+        {test.length &&
+          this.renderTimeLine()}
       </div>
     );
   }
